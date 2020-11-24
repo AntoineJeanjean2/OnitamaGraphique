e6a7c6975fb5b5ed05c33e8b1419cee6c5288344
antoi <antoi@LAPTOP-6ULMQBTH>
2020-11-24 11:14:14 +0100

    Création du dépôt

---
+++ Onitama/src/Joueur.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author antoi
+ */
+public class Joueur {
+    String nom;
+    String couleur;
+    Piece[] listePieces = new Piece[5];
+    Carte[] listeCartes = new Carte[2];
+}
---
+++ Onitama/src/Partie.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author antoi
+ */
+public class Partie {
+    Joueur[] listeJoueurs = new Joueur[2];
+    Grille grilleDeJeu;
+}
